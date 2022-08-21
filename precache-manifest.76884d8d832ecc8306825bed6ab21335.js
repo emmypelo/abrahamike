ebcb49cbdff6e2a88acc8c492062a1a4a4db00b6
emmypelo <emmypeloguns@gmail.com>
2022-08-21 15:03:15 -0700

    Updates

---
+++ precache-manifest.76884d8d832ecc8306825bed6ab21335.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b485dbf36f03d60c3d1400a891affb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0c0bfc1c1ae74b8e20",
+    "url": "/static/css/2.f2ac98b1.chunk.css"
+  },
+  {
+    "revision": "dcf5368da47e05072f74",
+    "url": "/static/css/main.ae45ad08.chunk.css"
+  },
+  {
+    "revision": "8a0c0bfc1c1ae74b8e20",
+    "url": "/static/js/2.3b989cf5.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/static/js/2.3b989cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf5368da47e05072f74",
+    "url": "/static/js/main.9ea6b0da.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9ea6b0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e84b34b756713675c2d",
+    "url": "/static/js/runtime-main.3b581e86.js"
+  },
+  {
+    "revision": "2b91e85bf888107baa9fad6b05d36cfc",
+    "url": "/static/media/Logo_white.2b91e85b.png"
+  },
+  {
+    "revision": "a2a801eef0ed05c0478f8d7d5c5340c7",
+    "url": "/static/media/about-img.a2a801ee.png"
+  },
+  {
+    "revision": "f4a68ca458fb523c01db2a499af9b125",
+    "url": "/static/media/model1.f4a68ca4.jpg"
+  },
+  {
+    "revision": "e758b0f14c9b91ffcc487940539d434d",
+    "url": "/static/media/model2.e758b0f1.jpg"
+  },
+  {
+    "revision": "6a621abf68cc58d073f18170409b61a9",
+    "url": "/static/media/model3.6a621abf.jpg"
+  },
+  {
+    "revision": "b71ec60ef38d90945b9bfb5f8e6bab57",
+    "url": "/static/media/model4.b71ec60e.jpg"
+  }
+]);
